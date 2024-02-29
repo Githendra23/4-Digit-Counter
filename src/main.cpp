3bc0a4db196d3dbb9a601598bd475df0e587eb3c
Githendra Perera <gpayagalage@gmail.com>
2024-02-29 17:57:14 +0100

    [+ ~] updated version of the 4 digits using an equation but the eqaution has a bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,29 @@
 #include <Arduino.h>
 
-#define BITSIZE 14
-#define TOTAL_DIGITS (sizeof(digits) / sizeof(digits[0]))
-#define TOTAL_SEGMENTS (sizeof(segments) / sizeof(segments[0]))
+#define TOTAL_SEGMENTS 7
+#define TOTAL_DIGITS 4
 
 int segments[] = {12, 11, 10, 9, 8, 7, 6};
 int digits[] = {A1, A2, A3, A4};
 
 void displayDigit(int number);
-byte getDigit(int number, int position);
 void turnOffAllDigits();
 
 void setup()
 {
-  for (unsigned int i = 0; i < TOTAL_SEGMENTS; i++)
+  for (int i = 0; i < TOTAL_SEGMENTS; i++)
   {
     pinMode(segments[i], OUTPUT);
   }
 
-  for (unsigned int i = 0; i < TOTAL_DIGITS; i++)
+  for (int i = 0; i < TOTAL_DIGITS; i++)
   {
     pinMode(digits[i], OUTPUT);
   }
   Serial.begin(9600);
 }
 
-int count = 990;
+int count = 80;
 void loop()
 {
   static uint32_t previousSecond = millis();
@@ -37,13 +35,13 @@ void loop()
     previousSecond = millis();
 
     count = (count + 1) % 10000;
-    Serial.println(count);
+    // Serial.println(count); // Commented out for optimization
   }
 }
 
 void displayDigit(int number)
 {
-  const int digitSegments[][7] = {
+  const int digitSegments[][TOTAL_SEGMENTS] = {
       {1, 1, 1, 1, 1, 1, 0}, // Define segments for digits 0-9
       {0, 1, 1, 0, 0, 0, 0},
       {1, 1, 0, 1, 1, 0, 1},
@@ -57,45 +55,22 @@ void displayDigit(int number)
 
   for (int i = TOTAL_DIGITS - 1; i >= 0; i--)
   {
-    byte digitNumber = getDigit(number, i);
+    byte digitNumber = (number / int(pow(10, TOTAL_DIGITS - 1 - i))) % 10;
 
     turnOffAllDigits();
 
-    if (i == 0)
-    {
-      Serial.println();
-      Serial.println();
-    }
-
-    for (unsigned int j = 0; j < TOTAL_SEGMENTS; j++)
+    for (int j = 0; j < TOTAL_SEGMENTS; j++)
     {
       digitalWrite(segments[j], digitSegments[digitNumber][j]);
     }
 
-    digitalWrite(digits[i], i == 3 || number / (int)pow(10, 3 - i) > 0 ? LOW : HIGH);
+    digitalWrite(digits[i], i == TOTAL_DIGITS - 1 || number / int(pow(10, TOTAL_DIGITS - 1 - i)) > 0 ? LOW : HIGH);
   }
-}
-
-byte getDigit(int number, int position)
-{
-  String binaryString = String(number);
-
-  while (binaryString.length() != 4)
-  {
-    binaryString = "0" + binaryString;
-  }
-
-  char digitChar = binaryString[position];
-  byte digitValue = digitChar - '0';
-
-  Serial.print(digitValue);
-
-  return digitValue;
 }
 
 void turnOffAllDigits()
 {
-  for (unsigned int j = 0; j < sizeof(digits) / sizeof(digits[0]); j++)
+  for (int j = 0; j < TOTAL_DIGITS; j++)
   {
     digitalWrite(digits[j], HIGH);
   }
